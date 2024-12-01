dc0a4feb969d82a034adf8238f286d96e3c7eace
SurajPandey <223236@kit.ac.in>
2024-12-01 18:18:32 +0530

    Update admin.py

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,12 +1,8 @@
 # todo/admin.py
-from django.contrib.auth.admin import UserAdmin 
-from .models import CustomUser 
 from django.contrib import admin
 from .models import Task # add this
 
 class TodoAdmin(admin.ModelAdmin):  # add this
     list_display = ('title', 'description', 'completed') # add this
-
     # Register your models here.
 admin.site.register(Task, TodoAdmin) # add this
-admin.site.register(CustomUser,UserAdmin)
