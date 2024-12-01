e12b67a2a6bde46b8dd87645afac544e08d58e06
SurajPandey <223236@kit.ac.in>
2024-12-01 17:59:33 +0530

    Update urls.py
    a bug

--- backend/todo_project/urls.py
+++ backend/todo_project/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include                 # add this
-from rest_framework import routers                    # add this
+from rest_framework import routers 
+from django.shortcuts import redirect          # add this
 from todo import views                            # add this
 
 router = routers.DefaultRouter()                      # add this
 router.register(r'todos', views.TaskView, 'todo')     # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                        # add this
+    path('admin/', admin.site.urls),
+    path('',lambda request: redirect('/admin/login/')),# add this
     path('api/', include(router.urls)),
-    path('api/auth/', include('rest_auth.urls')), path('api/auth/registration/', include('rest_auth.registration.urls')), path('accounts/', include('allauth.urls')),
+   
     ]
 
