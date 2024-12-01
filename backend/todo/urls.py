988b5bd9317352183cb3512242c43d77ac54bd4e
SurajPandey <223236@kit.ac.in>
2024-12-01 18:38:53 +0530

    Update urls.py

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
+from django.shortcuts import redirect
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('',lambda request:redirect(router.urls)),
 ]
